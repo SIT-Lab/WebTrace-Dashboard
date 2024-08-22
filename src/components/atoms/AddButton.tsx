150c37329f9aa6640c3d621bf617d4dc2e302d85
vuswltmd <sseung7367@gmail.com>
2024-08-22 19:31:07 +0900

    feat: AddButton 기능 수정
     - 버튼 높이, 너비, 굴곡을 선택할 수 있도록 수정

--- src/components/atoms/AddButton.tsx
+++ src/components/atoms/AddButton.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import AddImg from '../../assets/add.svg'
 
@@ -7,20 +7,28 @@ import AddImg from '../../assets/add.svg'
  */
 type ButtonClickEvent = React.MouseEvent<HTMLButtonElement, MouseEvent>
 
-const Container = styled.button<{ $color: string; $fontSize: string }>`
+const Container = styled.button<{ $color: string; $fontSize: string; $height: string; $width: string; $borderRadius: string }>`
   color: ${({ $color }) => $color};
   font-size: ${({ $fontSize }) => $fontSize};
   border: none;
-  border-radius: 8px;
+  border-radius: ${({ $borderRadius }) => $borderRadius};
   background-color: transparent;
   border: 2px solid ${({ $color }) => $color};
   padding: 8px;
   display: flex;
+  justify-content: center; /* 텍스트와 아이콘을 가운데 정렬 */
   align-items: center;
-  height: fit-content;
-  width: fit-content;
+  height: ${({ $height }) => $height};
+  width: ${({ $width }) => $width};
   cursor: pointer;
 `
+
+const ContentWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
 const TextArea = styled.span`
   padding: 4px 8px 4px 4px;
 `
@@ -34,6 +42,9 @@ interface AddButtonProps {
   text: string
   color: string
   fontSize?: string
+  height?: string
+  width?: string
+  borderRadius?: string
   onClick: (event: ButtonClickEvent) => void
 }
 
@@ -42,13 +53,26 @@ interface AddButtonProps {
  * @param {string} text - 버튼에 표시될 텍스트
  * @param {string} color - 버튼의 색상
  * @param {string} [fontSize='16px'] - 텍스트의 폰트 크기
+ * @param {string} [height='auto'] - 버튼의 높이
+ * @param {string} [width='fit-content'] - 버튼의 너비
+ * @param {string} [borderRadius='8px'] - 버튼의 보더 레디어스 (굴곡)
  * @param {function} onClick - 버튼 클릭 이벤트 핸들러
  */
-export const AddButton = ({ text, color = 'gray', fontSize = '16px', onClick }: AddButtonProps) => {
+export const AddButton = ({ text, color = 'gray', fontSize = '16px', height = 'auto', width = 'fit-content', borderRadius = '8px', onClick }: AddButtonProps) => {
   return (
-    <Container type="button" onClick={onClick} $color={color} $fontSize={fontSize}>
-      {<TextArea>{text}</TextArea>}
-      <AddIcon src={AddImg} color={color}></AddIcon>
+    <Container
+      type="button"
+      onClick={onClick}
+      $color={color}
+      $fontSize={fontSize}
+      $height={height}
+      $width={width}
+      $borderRadius={borderRadius}
+    >
+      <ContentWrapper>
+        <TextArea>{text}</TextArea>
+        <AddIcon src={AddImg} color={color} />
+      </ContentWrapper>
     </Container>
   )
 }
