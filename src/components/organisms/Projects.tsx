ff8e0d2eb843f2313afbab3b30c9507d2f51d44f
vuswltmd <sseung7367@gmail.com>
2024-08-22 19:47:17 +0900

    feat: Projects 컴포넌트의  프로젝트 ID와 이름을 전역 상태로 저장하는 기능 추가

--- src/components/organisms/Projects.tsx
+++ src/components/organisms/Projects.tsx
@@ -6,9 +6,12 @@ import { COLORS } from '../../styles/colors'
 import { useNavigate } from 'react-router-dom'
 import { IconButton } from '../atoms/IconButton'
 import deleteIcon from '../../assets/trash.svg'
+import { useProject } from '../../context/ProjectContext'
 
 const Container = styled.div`
-  margin: 32px 42px;
+  margin-left: 32px;
+  margin-right: 32px;
+  margin-top: 10px;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
@@ -17,9 +20,6 @@ const Container = styled.div`
   min-width: 300px;
 `
 
-/**
- * Item이란 각 프로젝트를 표시하는 개별 항목
- */
 const Item = styled.div`
   padding: 20px;
   display: flex;
@@ -31,17 +31,22 @@ const ItemInfoBox = styled.div`
   flex-direction: column;
 `
 
+/**
+ * 스타일이 적용된 16px 간격
+ */
+const Gap16 = styled.div`
+  padding: 16px;
+`
+
 const GrayText = styled.span`
   color: ${COLORS.gray02};
 `
-
 /**
  * ProjectsProps 인터페이스: Projects 컴포넌트의 props를 정의
  */
 export interface ProjectsProps {
   ownerID: string
 }
-
 /**
  * Projects 컴포넌트
  * @param {ProjectsProps} props - Projects 컴포넌트에 전달될 props
@@ -51,6 +56,7 @@ function Projects(props: ProjectsProps) {
   const [projects, setProjects] = useState<ProjectData[]>([])
   const [loading, setLoading] = useState(true)
   const navigate = useNavigate()
+  const { setSelectedProjectId, setSelectedProjectName } = useProject()
 
   /**
    * 프로젝트 데이터를 가져오는 useEffect 훅
@@ -68,39 +74,52 @@ function Projects(props: ProjectsProps) {
       }
     }
     fetchProjects()
-  }, [])
+  }, [props.ownerID])
 
   return (
-    <Container>
-      {projects.length > 0 ? (
-        projects.map((proj, i) => (
-          <Item key={i} onClick={() => navigate(`/${proj.id}`)}>
-            <ItemInfoBox>
-              <span>{proj.title}</span>
-              <GrayText>{`ID: ${proj.id}`}</GrayText>
-            </ItemInfoBox>
-            <IconButton
-              iconPath={deleteIcon}
-              onClick={async (e) => {
-                e.stopPropagation()
-                const response = confirm('Do you want to permanently delete the project?')
-                if (response) {
-                  console.log(proj.id)
-                  if (await deleteProject(proj.id)) {
-                    alert('You have successfully deleted the project.')
-                    location.reload()
-                  } else {
-                    alert('Failed to delete the project.')
-                  }
-                }
+    <>
+      <Gap16 />
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginLeft: '42px' }}>
+        <b style={{ fontSize: '18px', fontWeight: 'bold' }}>Project</b>
+      </div>
+      <Container>
+        {projects.length > 0 ? (
+          projects.map((proj, i) => (
+            <Item
+              key={i}
+              onClick={() => {
+                setSelectedProjectId(proj.id) // 프로젝트 ID를 전역 상태로 저장
+                setSelectedProjectName(proj.title) // 프로젝트 이름을 전역 상태로 저장
+                navigate(`/${proj.id}`)
               }}
-            />
-          </Item>
-        ))
-      ) : (
-        <div>{loading ? 'Loading...' : 'No Projects found.'}</div>
-      )}
-    </Container>
+            >
+              <ItemInfoBox>
+                <span>{proj.title}</span>
+                <GrayText>{`ID: ${proj.id}`}</GrayText>
+              </ItemInfoBox>
+              <IconButton
+                iconPath={deleteIcon}
+                onClick={async (e) => {
+                  e.stopPropagation()
+                  const response = confirm('Do you want to permanently delete the project?')
+                  if (response) {
+                    console.log(proj.id)
+                    if (await deleteProject(proj.id)) {
+                      alert('You have successfully deleted the project.')
+                      location.reload()
+                    } else {
+                      alert('Failed to delete the project.')
+                    }
+                  }
+                }}
+              />
+            </Item>
+          ))
+        ) : (
+          <div>{loading ? 'Loading...' : 'No Projects found.'}</div>
+        )}
+      </Container>
+    </>
   )
 }
 
